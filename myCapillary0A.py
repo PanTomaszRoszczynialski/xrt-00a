7a21160f53c67c126c4f46da45344e4d899fd8ab
Tomasz Roszczynialski <roszcz@gmail.com>
2015-02-08 03:50:07 +0100

    bigger rIn can see diff

--- myCapillary0A.py
+++ myCapillary0A.py
@@ -34,10 +34,10 @@ xzPrimeMax = 3.
 class BentCapillary(roe.OE):
     def __init__(self, *args, **kwargs):
         self.rSample = kwargs.pop('rSample')
-        self.entranceAlpha = kwargs.pop('entranceAlpha')*2
+        self.entranceAlpha = kwargs.pop('entranceAlpha')
         self.f = kwargs.pop('f')
-        self.r0in = kwargs.pop('rIn')/2
-        self.r0out = kwargs.pop('rOut')*4
+        self.r0in = kwargs.pop('rIn')*2
+        self.r0out = kwargs.pop('rOut')/2
         roe.OE.__init__(self, *args, **kwargs)
 
         s0 = self.f - self.rSample * np.cos(self.entranceAlpha)
