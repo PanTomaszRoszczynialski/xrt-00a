9eb6566e1ebcc33e1067cfdabe59b439e68459dd
Tomasz Roszczynialski <roszcz@gmail.com>
2015-02-08 02:52:22 +0100

    smaller rOut, no vis diff.

--- myCapillary0A.py
+++ myCapillary0A.py
@@ -37,7 +37,7 @@ class BentCapillary(roe.OE):
         self.entranceAlpha = kwargs.pop('entranceAlpha')*2
         self.f = kwargs.pop('f')
         self.r0in = kwargs.pop('rIn')/2
-        self.r0out = kwargs.pop('rOut')*10
+        self.r0out = kwargs.pop('rOut')*4
         roe.OE.__init__(self, *args, **kwargs)
 
         s0 = self.f - self.rSample * np.cos(self.entranceAlpha)
