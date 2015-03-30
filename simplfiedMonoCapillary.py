aeac256f5c46fecd2efae9f564e4e77e50c6b749
Tomasz Roszczynialski <roszcz@gmail.com>
2015-03-30 20:50:11 +0200

    Incidence angle caxis seems to not be scaled properly?

--- simplfiedMonoCapillary.py
+++ simplfiedMonoCapillary.py
@@ -179,7 +179,7 @@ def main():
         xaxis=xrtp.XYCAxis(r'$z$', 'mm', data=raycing.get_z, bins=256, ppb=2, limits = None),
         yaxis=xrtp.XYCAxis(r"$x$", 'mm', data=raycing.get_x, bins=256, ppb=2, limits = None),
 #        caxis='category', 
-        caxis=xrtp.XYCAxis("Reflections", '#',data=raycing.get_reflection_number, bins=256, ppb=2),
+        caxis=xrtp.XYCAxis("incidence angle", 'dg',data=raycing.get_incidence_angle, bins=256, ppb=2),
         beamState='beamFSM2', title='FSM2_Cat', aspect='auto')
     plot.baseName = 'phaseSearch'
     plot.saveName = plot.baseName + '.png'
