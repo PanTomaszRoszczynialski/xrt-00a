5a8155a31b7c6adea31e12f8285b6bf776dcecdf
Tomasz Roszczynialski <roszcz@gmail.com>
2015-09-01 15:07:33 +0200

    Transmission still seems to be unrelated to width of passed object..

--- object_testing.py
+++ object_testing.py
@@ -82,7 +82,7 @@ def build_beamline(nrays=1000):
     shape = [(0.1*v[0], 0.1*v[1]) for v in shape]
 
     # Set position and thickness
-    limPhysY = [39.8, 39.95]
+    limPhysY = [37.8, 39.95]
     limPhysX = [-2.0, 2.0]
 
     # Tested material
