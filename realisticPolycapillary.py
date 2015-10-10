6d2fa93ca00e27a98cbc46917d4bfc9cf4764b0e
Mircea Eliade <roszcz@gmail.com>
2015-10-10 15:42:07 +0200

    1.5e-8

--- realisticPolycapillary.py
+++ realisticPolycapillary.py
@@ -87,7 +87,7 @@ ypin    = 155 - pinlen        # Optical path position
 # for FittedSource as well, and critical angle should not be guessed
 # Source parameters
 _rays       = 20
-_tmp_factor = 0.00015
+_tmp_factor = 0.000000015
 # x-direction
 distx       = 'flat'
 dx          = rIn*1.5
