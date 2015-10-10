05cd0ff8196d898bf7e85ff6992662fd7335c370
Mircea Eliade <roszcz@gmail.com>
2015-10-10 16:10:45 +0200

    Parallel beam for each capillary

--- realisticPolycapillary.py
+++ realisticPolycapillary.py
@@ -87,7 +87,7 @@ ypin    = 155 - pinlen        # Optical path position
 # for FittedSource as well, and critical angle should not be guessed
 # Source parameters
 _rays       = 20
-_tmp_factor = 0.000000015
+_tmp_factor = 1e-18
 # x-direction
 distx       = 'flat'
 dx          = rIn*1.5
